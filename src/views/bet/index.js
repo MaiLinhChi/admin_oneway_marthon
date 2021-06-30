4432d51e64389aa361da72c48a7ee45e65a1ce9b
lamtanphiho <lamtanphiho1@gmail.com>
2021-06-30 17:18:41 +0700

    update bet result

--- src/views/bet/index.js
+++ src/views/bet/index.js
@@ -10,6 +10,7 @@ import {
     CLink
 } from '@coreui/react'
 import moment from 'moment'
+import numeral from 'numeral'
 import HTTP from 'src/controller/API/HTTP'
 import { ellipsisAddress } from 'src/helper/addressHelper';
 import {
@@ -20,12 +21,13 @@ import Spinner from 'src/views/base/spinner';
 
 const getBadge = status => {
     switch (status) {
+        case 'UP':
         case 1: return 'success'
         default: return 'danger'
     }
 }
 
-const fields = ['user','txhash', 'gameId', 'betAmount', 'winAmount', 'type', 'createdAt']
+const fields = ['user','txhash', 'gameId', 'lockedPrice', 'closePrice', 'result', 'betSide', 'betAmount', 'winAmount', 'type', 'createdAt']
 
 const View = () => {
   const [loading, setLoading] = useState(false);
@@ -56,6 +58,7 @@ const View = () => {
                   items={bets}
                   fields={fields}
                   striped
+                  columnFilter
                   itemsPerPage={20}
                   pagination
                   scopedSlots = {{
@@ -65,7 +68,26 @@ const View = () => {
                     'txhash': (item) => (
                           <td><CLink href={detectTransaction(item.txhash)} target="_blank">{`${ellipsisAddress(item.txhash)}`}</CLink></td>
                       ),
-                    'type':
+                    'lockedPrice': (item) => (
+                          <td>{numeral(item.lockedPrice/100).format('0,0.00')}</td>
+                      ),
+                    'closePrice': (item) => (
+                          <td>{item.closePrice && item.closePrice != 0 ? numeral(item.closePrice/100).format('0,0.00') : '--/--'}</td>
+                      ),
+                    'result': (item) => (
+                          <td>{item.result ? <CBadge color={getBadge(item.result)}>
+                              {item.result}
+                          </CBadge> : '--/--'}</td>
+                      ),
+                    'betSide':
+                        (item)=>(
+                            <td>
+                              <CBadge color={getBadge(item.betSide)}>
+                                {item.betSide}
+                              </CBadge>
+                            </td>
+                        ),
+                      'type':
                         (item)=>(
                             <td>
                               <CBadge color={getBadge(item.type)}>
