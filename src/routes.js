59484896fe8fc1854d931752905572a2b14396ac
lamtanphiho <lamtanphiho1@gmail.com>
2021-07-01 14:23:46 +0700

    handle walletconnect

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import React from 'react'
 const Account = React.lazy(() => import('./views/account'));
 const Setting = React.lazy(() => import('./views/setting'));
 const Bet = React.lazy(() => import('./views/bet'));
+const Game = React.lazy(() => import('./views/game'));
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/account', name: 'Account', component: Account },
   { path: '/setting', name: 'Setting', component: Setting },
+  { path: '/games', name: 'Games', component: Game },
   { path: '/bets', name: 'Bets', component: Bet },
 ]
 
---
+++ src/views/game/index.js
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from 'react'
+import {
+  CBadge,
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CDataTable,
+  CRow,
+    CLink
+} from '@coreui/react'
+import moment from 'moment'
+import numeral from 'numeral'
+import HTTP from 'src/controller/API/HTTP'
+import { ellipsisAddress } from 'src/helper/addressHelper';
+import {
+    detectAddress,
+    detectTransaction
+} from 'src/common/function';
+import Spinner from 'src/views/base/spinner';
+
+const getBadge = status => {
+    switch (status) {
+        case 'UP':
+        case 1: return 'success'
+        default: return 'danger'
+    }
+}
+
+const fields = ['gameId', 'txhash', 'lockedPrice', 'closePrice', 'result', 'totalbetAmount', 'comm', 'createdAt']
+
+const View = () => {
+  const [loading, setLoading] = useState(false);
+  const [bets, setBets] = useState([]);
+  const [totalComm, setTotalComm] = useState(0);
+  const [totalbetAmount, settotalbetAmount] = useState(0);
+
+    const run = async () => {
+        setLoading(true)
+        const res = await HTTP.fetchData('/games', 'GET', {sort: 'desc', limit: 10000}, null);
+        setBets(res.data)
+        setTotalComm(res.totalComm)
+        settotalbetAmount(res.totalbetAmount)
+        setLoading(false)
+    }
+    useEffect(() => {
+        run()
+    }, [])
+  return (
+      loading
+          ? <Spinner />
+          :
+      <CRow>
+        <CCol xs="12" lg="12">
+          <CCard>
+            <CCardHeader>
+              Bets
+                <div className="text-muted float-right mt-1">
+                    <p className="float-left"> Total Bet Amount: <b>{numeral(totalbetAmount).format('0,0.00')}</b> - Total Commission: <b>{numeral(totalComm).format('0,0.00')}</b></p>
+                </div>
+            </CCardHeader>
+            <CCardBody>
+              <CDataTable
+                  items={bets}
+                  fields={fields}
+                  striped
+                  columnFilter
+                  itemsPerPage={20}
+                  pagination
+                  scopedSlots = {{
+                    'txhash': (item) => (
+                          <td><CLink href={detectTransaction(item.txhash)} target="_blank">{`${ellipsisAddress(item.txhash)}`}</CLink></td>
+                      ),
+                    'lockedPrice': (item) => (
+                          <td>{numeral(item.lockedPrice/100).format('0,0.00')}</td>
+                      ),
+                    'closePrice': (item) => (
+                          <td>{item.closePrice && item.closePrice != 0 ? numeral(item.closePrice/100).format('0,0.00') : '--/--'}</td>
+                      ),
+                    'result': (item) => (
+                          <td>{item.result ? <CBadge color={getBadge(item.result)}>
+                              {item.result}
+                          </CBadge> : '--/--'}</td>
+                      ),
+                      'createdAt': (item)=>(
+                          <td>{moment(item.createdAt).format('YYYY-MM-DD HH:mm:ss')}</td>
+                      ),
+                  }}
+
+              />
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
+  )
+}
+
+export default View
