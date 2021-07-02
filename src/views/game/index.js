43a6e92413d00dee36ed04ab1ef724c7e5c835ed
lamtanphiho <lamtanphiho1@gmail.com>
2021-07-02 15:51:45 +0700

    h

--- src/views/game/index.js
+++ src/views/game/index.js
@@ -84,7 +84,7 @@ const View = () => {
                           </CBadge> : '--/--'}</td>
                       ),
                       'comm': (item) => (
-                          <td>{numeral(item.comm).format('0,0.00000000')}</td>
+                          <td>{numeral(item.comm).format('0,0.00')}</td>
                       ),
                       'createdAt': (item)=>(
                           <td>{moment(item.createdAt).format('YYYY-MM-DD HH:mm:ss')}</td>
