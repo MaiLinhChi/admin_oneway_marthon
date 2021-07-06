0eefbed918ab69b2f65cef27267a7a8424717eef
lamtanphiho <lamtanphiho1@gmail.com>
2021-07-06 10:21:01 +0700

    h

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -30,7 +30,7 @@ const Login = () => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
   const checkUserAuthentication = async () => {
-    localStorage.setItem(userAuth, null)
+    localStorage.setItem('userAuth', null)
     // let userAuth = await getDataLocal('userAuth')
     // if (userAuth) {
     //   history.push('/')
